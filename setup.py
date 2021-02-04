2e42e34b6693bc62dfae2fffca620db8cb21cc17
Kumar Dhruv <54675483+Technology-cell@users.noreply.github.com>
2021-02-04 10:31:46 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name='HQProxy',
+    version='1.0.0',    
+    description='HQ Trivia Proxy UK & USA',
+    url='',
+    author='Technology-cell',
+    author_email='kd2346751@gmail.com',
+    packages=['HQProxy'],
+    install_requires=['free-proxy','urllib3','socket'],
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
