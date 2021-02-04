be109c403022835a264e270881fa22e216d15000
Kumar Dhruv <54675483+Technology-cell@users.noreply.github.com>
2021-02-04 12:33:49 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 import setuptools
 
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+with open("README.md", "r") as fh:
+	long_description = fh.read()
 
 setuptools.setup(
     name='HQProxy',
-    version='1.1.0',    
+    version='2.0.0',    
     description='HQ Trivia Proxy UK & USA',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -15,7 +13,7 @@ setuptools.setup(
     author='Technology-cell',
     author_email='kd2346751@gmail.com',
     packages=['HQProxy'],
-    install_requires=['free-proxy','urllib3','socket'],
+    install_requires=['free-proxy','urllib3'],
     classifiers=[
         'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
